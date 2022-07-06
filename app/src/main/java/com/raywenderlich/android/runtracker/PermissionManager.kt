c9407335d31f31b4681fbfe72fd60188db033fbb
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-06 11:37:57 +0500

    ask for permission

--- app/src/main/java/com/raywenderlich/android/runtracker/PermissionManager.kt
+++ app/src/main/java/com/raywenderlich/android/runtracker/PermissionManager.kt
@@ -1,12 +1,14 @@
 package com.raywenderlich.android.runtracker
 
 import android.Manifest
+import android.os.Build
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class PermissionManager(
     activity: AppCompatActivity,
-    private val locationProvider: LocationProvider
+    private val locationProvider: LocationProvider,
+    private val stepCounter: StepCounter
 ) {
     private val locationPermissionProvider = activity
         .registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
@@ -15,7 +17,22 @@ class PermissionManager(
             }
         }
 
+    private val activityRecognitionPermissionProvider = activity
+        .registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                stepCounter.setupStepCounter()
+            }
+        }
+
     fun requestUserLocation() {
         locationPermissionProvider.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    fun requestActivityRecognition() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            activityRecognitionPermissionProvider.launch(Manifest.permission.ACTIVITY_RECOGNITION)
+        } else {
+            stepCounter.setupStepCounter()
+        }
     }
 }
