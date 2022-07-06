c8151c609aad53f629057c689a33cd9fa803442f
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-06 08:35:42 +0500

    getting the user's location

---
+++ app/src/main/java/com/raywenderlich/android/runtracker/LocationProvider.kt
@@ -0,0 +1,26 @@
+package com.raywenderlich.android.runtracker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+
+@SuppressLint("MissingPermission")
+class LocationProvider(private val activity: AppCompatActivity) {
+
+    private val client
+        by lazy { LocationServices.getFusedLocationProviderClient(activity) }
+
+    private val locations = mutableListOf<LatLng>()
+
+    val liveLocations = MutableLiveData<LatLng>()
+
+    fun getUserLocation() {
+        client.lastLocation.addOnSuccessListener { location ->
+            val latLng = LatLng(location.latitude, location.longitude)
+            locations.add(latLng)
+            liveLocations.value = latLng
+        }
+    }
+}
