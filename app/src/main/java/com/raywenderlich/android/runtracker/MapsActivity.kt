e3fee454798b4c7beef1d70642cdfd40de1d74c6
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-06 08:21:15 +0500

    add Google Maps API key

--- app/src/main/java/com/raywenderlich/android/runtracker/MapsActivity.kt
+++ app/src/main/java/com/raywenderlich/android/runtracker/MapsActivity.kt
@@ -74,11 +74,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
    * installed Google Play services and returned to the app.
    */
   override fun onMapReady(googleMap: GoogleMap) {
-    mMap = googleMap
+    map = googleMap
 
     // Add a marker in Sydney and move the camera
     val sydney = LatLng(-34.0, 151.0)
-    mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+    map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
   }
 }
