8f64250bbf77acc659626fe199be101792dee588
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-06 09:26:16 +0500

    show user movement

--- app/src/main/java/com/raywenderlich/android/runtracker/LocationProvider.kt
+++ app/src/main/java/com/raywenderlich/android/runtracker/LocationProvider.kt
@@ -1,10 +1,16 @@
 package com.raywenderlich.android.runtracker
 
 import android.annotation.SuppressLint
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import kotlin.math.roundToInt
 
 @SuppressLint("MissingPermission")
 class LocationProvider(private val activity: AppCompatActivity) {
@@ -14,15 +20,50 @@ class LocationProvider(private val activity: AppCompatActivity) {
 
     private val locations = mutableListOf<LatLng>()
 
-    val liveLocations = MutableLiveData<LatLng>()
+    private var distance = 0
+
+    val liveLocation = MutableLiveData<LatLng>()
+    val liveLocations = MutableLiveData<List<LatLng>>()
+    val liveDistance = MutableLiveData<Int>()
 
     fun getUserLocation() {
         client.lastLocation.addOnSuccessListener { location ->
             if (location != null) {
                 val latLng = LatLng(location.latitude, location.longitude)
                 locations.add(latLng)
-                liveLocations.value = latLng
+                liveLocation.value = latLng
             }
+        }
+    }
+
+    fun trackUser() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+
+        client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
+    fun stopTracking() {
+        client.removeLocationUpdates(locationCallback)
+        locations.clear()
+        distance = 0
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            val currentLocation = result.lastLocation
+            val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+
+            val lastLocation = locations.lastOrNull()
+
+            if (lastLocation != null) {
+                distance += SphericalUtil.computeDistanceBetween(lastLocation, latLng).roundToInt()
+                liveDistance.value = distance
+            }
+
+            locations.add(latLng)
+            liveLocations.value = locations
         }
     }
 }
